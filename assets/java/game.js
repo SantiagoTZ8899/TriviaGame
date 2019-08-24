8d1a7f3ee098664f7858952b6fc72f27b6c0266f
Santiago Tolosa Zegarra <santiagot-z@santiagos-mbp.home>
2019-08-23 22:36:54 -0500

    html divs and links to libraries and files, pse js

---
+++ assets/java/game.js
@@ -0,0 +1,22 @@
+// Display questions - list of questions
+//     array of abjects
+//         question object, plus 4 answers
+
+//     select a question to start game    
+
+//     start a timer of 20 seconds
+
+//     allow user to choose an answer with click
+
+//     function for when correct - add point to correct score
+//     function when incorrect - add point to incorrect score
+//     ran out of time - add point to incorrect score
+//     show if answer correct or incorrect after each question
+
+//     once user select an answer, move to next question and restart timer
+
+//     show user end-of-game screen with corrects, incorrects, and total score
+
+//     after last question reset game with a button (not refresh page)
+//         reset scores
+//         restart questions
